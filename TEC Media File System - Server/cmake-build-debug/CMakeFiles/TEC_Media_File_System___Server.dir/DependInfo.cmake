1d89de5d291e6714703b635a2e13895a6914ce89
davepj007 <37641342+davepj007@users.noreply.github.com>
2018-11-25 13:16:23 -0600

    Server Agregado

---
+++ TEC Media File System - Server/cmake-build-debug/CMakeFiles/TEC_Media_File_System___Server.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/davepj007/CLionProjects/TEC Media File System - Server/server.cpp" "/home/davepj007/CLionProjects/TEC Media File System - Server/cmake-build-debug/CMakeFiles/TEC_Media_File_System___Server.dir/server.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
